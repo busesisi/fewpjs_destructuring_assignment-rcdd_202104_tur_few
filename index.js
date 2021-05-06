6d53516636697fcea3de869c6f96a85dea385f8c
Buse Şentürk <buse.senturk@outlook.com>
2021-05-06 15:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const [red, orange, yellow, green, blue, indigo, violet] = colors;
 const [r, o, y, g, b, , v] = colors;
+const [ , , , , , indg, ,] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
