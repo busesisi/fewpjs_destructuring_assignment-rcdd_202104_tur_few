c70631959b446d729d667fcd22e1a8c718eaaef7
Buse Şentürk <buse.senturk@outlook.com>
2021-05-06 15:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, dolly, babe, little] = farmAnimals.split(' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
